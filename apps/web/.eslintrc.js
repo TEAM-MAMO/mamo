5ab480746636ba5a2c2fdc6be66f07ba9a2c7443
dgd03146 <ibory1220@naver.com>
2024-02-29 15:59:30 +0000

    chore: set up styleX for web workspace

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,6 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  plugins: ['@stylexjs'],
+  rules: {
+    '@stylexjs/valid-styles': 'error',
+  },
   extends: ['@repo/eslint-config/eslint-next'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
-};
+
+const path = require('path');
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+
+module.exports = stylexPlugin({
+  aliases: {
+    '@/*': [path.join(__dirname, '*')],
+  },
+  rootDir: __dirname,
+  transpilePackages: ['@repo/ui'],
+})({});
