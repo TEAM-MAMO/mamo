adb4fb1a80cea1b208d76c91f6c354d9e8cdbc79
thisyujeong <this.yujeong@gmail.com>
2024-04-07 03:40:48 +0900

    feat: InputNumber 컴포넌트 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -5,6 +5,7 @@ export { BottomSheet, type BottomSheetProps } from './BottomSheet/BottomSheet';
 export { Fab, type FabProps } from './Fab/Fab';
 export { Divider, type DividerProps } from './Divider/Divider';
 export { Input, type InputProps } from './Input/Input';
+export { InputNumber, type InputNumberProps } from './InputNumber/InputNumber';
 export { Textarea, type TextareaProps } from './Textarea/Textarea';
 export {
   Typography,
