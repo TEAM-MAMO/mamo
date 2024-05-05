769509d17bbc0abe13f0b51ed1922d6515a6e247
dgd03146 <ibory1220@naver.com>
2024-05-05 19:06:29 +0100

    comment: 사용하지 않는 주석 삭제

--- packages/ui/src/components/Accordion/AccordionContent.css.ts
+++ packages/ui/src/components/Accordion/AccordionContent.css.ts
@@ -38,27 +38,3 @@ globalStyle(`${contentChild}:not(:last-child)`, {
   paddingBottom: "16px",
   borderBottom: "0.5px solid #EFEFEF",
 });
-
-// export const content = recipe({
-//   base: style([
-//     sprinkles({
-//       display: 'flex',
-//       flexDirection: 'column',
-//       gapY: '6x',
-//       overflow: 'hidden',
-//     }),
-//     {
-//       height: contentHeightVar,
-//       transitionProperty: 'height',
-//       transitionDuration: '200ms',
-//       transitionDelay: '100ms',
-//       transitionTimingFunction: 'cubic-bezier(0.37, 0, 0.63, 1)',
-//     },
-//   ]),
-//   variants: {
-//     open: {
-//       true: sprinkles({ paddingTop: '6x' }),
-//       false: sprinkles({ paddingTop: '0' }),
-//     },
-//   },
-// });
